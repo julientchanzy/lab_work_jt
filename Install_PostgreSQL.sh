5de54e6d72b6cc5c6ce80f803cc1cd626331b2fa
julientchanzy <88672741+julientchanzy@users.noreply.github.com>
2023-02-10 22:43:23 -0500

    Create Install_PostgreSQL.sh

---
+++ Install_PostgreSQL.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install PostgreSQL 13
+sshpass -p "${SSH_PASSWORD}" ssh -o StrictHostKeyChecking=no ${SSH_USERNAME}@172.16.0.239 << EOF
+sudo yum install -y https://download.postgresql.org/pub/repos/yum/13/redhat/rhel-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+sudo yum install -y postgresql13-server postgresql13-contrib
+sudo /usr/pgsql-13/bin/postgresql-13-setup initdb
+sudo systemctl start postgresql-13.service
+EOF
