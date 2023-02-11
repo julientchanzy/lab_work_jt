d737699c0ca9d73e27bdcc154620291b0ff2649c
julientchanzy <88672741+julientchanzy@users.noreply.github.com>
2023-02-10 23:11:23 -0500

    Update check_pg.sh

--- check_pg.sh
+++ check_pg.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Set PostgreSQL connection information
-PGUSER=yourusername
-PGPASSWORD=yourpassword
+PGUSER=postgres
 PGHOST=localhost
 PGPORT=5432
-PGDATABASE=yourdatabase
+PGDATABASE=postgres
 
 # Connect to PostgreSQL and query pg_stat_activity and pg_stat_user_tables
 psql -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE -c "SELECT * FROM pg_stat_activity;"
